b823c5574b3184f8cd3e513c93a49351691dc3c4
rps379 <rudrasrawat379@gmail.com>
2026-09-12 15:34:35 +0530

    added preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,35 +1,30 @@
 import re
-def read_corpus(filename):
-    with open(filename, "r", encoding="utf-8") as file:
-        data = file.read()
+from nltk import word_tokenize
+from nltk.corpus import stopwords
 
-    documents = re.findall(r"<DOC>(.*?)</DOC>", data, re.DOTALL)
-    corpus={}
-    for doc in documents:
-        docid = re.search(r"<DOCID>(.*?)</DOCID>", doc).group(1)
-        category = re.search(r"<CATEGORY>(.*?)</CATEGORY>", doc).group(1)
-        title = re.search(r"<TITLE>(.*?)</TITLE>", doc).group(1)
-        text = re.search(r"<TEXT>(.*?)</TEXT>", doc, re.DOTALL).group(1)
+from nltk.stem import PorterStemmer
 
-        corpus[docid] = {
-            "category": category,
-            "title": title,
-            "text": text
-        }
+stemmer=PorterStemmer()
 
-    return corpus
+stop_words = set(stopwords.words('english'))
 
-def main():
-    corpus = read_corpus("corpus_100.txt")
 
-    print("Total documents:", len(corpus))
+def preprocess(text):
+    text=text.lower() #convert all to lowercase
 
-    for docid, document in corpus.items():
-        print("\nDocument ID:", docid)
-        print("Category:", document["category"])
-        print("Title:", document["title"])
-        print("Text:", document["text"][:100], "...")
+    text=re.sub(r"[^a-z0-9\s]", "", text)  #remove punctuations from text
 
+    tokens=word_tokenize(text)  #split into tokens
+
+    tokens=[word for word in tokens if word not in stop_words]  #remove common words
+
+    tokens=[stemmer.stem(word) for word in tokens]  #stemm the tokens
+
+    return tokens
 
 if __name__ == "__main__":
-    main()
+    text = "Men's Cotton Crew Neck T-Shirt - Black. Made from 100% cotton!"
+
+    result = preprocess(text)
+
+    print(result)
